5ba43c55e2958004eb9c83c82a6750574cbe63a7
Manuel Alejandro Moscoso Domínguez <manuel@mobiquos.cl>
2023-03-16 18:26:31 -0300

    Hola Mundo en programa .NET C#

---
+++ EjemploVisualStudio2022/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemploVisualStudio2022
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo, ejemplo de nuevo codigo o ejercicio");
+            Console.ReadLine();
+        }
+    }
+}
